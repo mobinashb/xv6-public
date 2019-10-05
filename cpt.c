a195ce927c7662ea11dfdc22281805f4f1b95c8f
AMorteza1376 <Amirhosein.ut1376@gmail.com>
2019-10-05 13:32:53 +0330

    Edit cpt command

--- cpt.c
+++ cpt.c
@@ -2,12 +2,11 @@
 #include "user.h"
 #include "fcntl.h"
 
+char buf[512];
+
 int
 main(int argc, char **argv)
 {
-	// printf(1, "argc:  %d\n", argc);
-	// printf(1, "argv1:  %s\n", argv[1]);
-	// printf(1, "argv2:  %s\n", argv[2]);
 	if(argc <= 1){
 		printf(2, "error cpt: missing file operand\n");
 	    exit();
@@ -18,9 +17,20 @@ main(int argc, char **argv)
         printf(1, "error cpt: create file failed\n");
         exit();
     }
-    // int a;
-    // scanf("%d", &a);
+    
+    int max = sizeof(buf);
+    int cc, i;
+    char c;
+    for(i=0; i+1 < max; ){
+        cc = read(0, &c, 1);
+        if(cc < 1)
+            break;
+        buf[i++] = c;
+        if(c == '\n' || c == '\r')
+            break;
+    }
+    if(write(fd, buf, i) != i)
+          printf(1, "error: write %s to %s failed\n", buf, buffer);              
     close(fd);
-	
-    return 0;
+    exit();
 }
