a16b7174de72c7a05bf32d6a2f8797978fe41994
AMorteza1376 <Amirhosein.ut1376@gmail.com>
2019-10-05 13:55:10 +0330

    Add cpt multi arg

--- cpt.c
+++ cpt.c
@@ -10,27 +10,41 @@ main(int argc, char **argv)
 	if(argc <= 1){
 		printf(2, "error cpt: missing file operand\n");
 	    exit();
-  	}
-	char* buffer = argv[1];
- 	int fd = open(buffer, O_CREATE | O_RDWR);
-    if(fd < 0) {
-        printf(1, "error cpt: create file failed\n");
-        exit();
+  	}else if (argc == 2){
+        char* buffer = argv[1];
+        int fd = open(buffer, O_CREATE | O_RDWR);
+        if(fd < 0) {
+            printf(2, "error cpt: failed to create file %s\n", buffer);
+            exit();
+        }
+        int max = sizeof(buf);
+        int cc, i;
+        char c;
+        for(i=0; i+1 < max; ){
+            cc = read(0, &c, 1);
+            if(cc < 1)
+                break;
+            buf[i++] = c;
+            if(c == '\n' || c == '\r')
+                break;
+        }
+        if(write(fd, buf, i) != i)
+              printf(2, "error: write %s to %s failed\n", buf, buffer);              
+        close(fd);   
+    }else if (argc == 3){
+        char* in_buf = argv[1];
+        int in_fd = open(in_buf, O_RDWR);
+        if(in_fd < 0) {
+            printf(2, "error cpt: failed to open %s no such file\n", in_buf);
+            exit();
+        }
+
+        char* out_buf = argv[2];
+        int out_fd = open(out_buf, O_CREATE | O_RDWR);
+        if(out_fd < 0) {
+            printf(2, "error cpt: failed to create file %s\n", out_buf);
+            exit();
+        }
     }
-    
-    int max = sizeof(buf);
-    int cc, i;
-    char c;
-    for(i=0; i+1 < max; ){
-        cc = read(0, &c, 1);
-        if(cc < 1)
-            break;
-        buf[i++] = c;
-        if(c == '\n' || c == '\r')
-            break;
-    }
-    if(write(fd, buf, i) != i)
-          printf(1, "error: write %s to %s failed\n", buf, buffer);              
-    close(fd);
     exit();
 }
